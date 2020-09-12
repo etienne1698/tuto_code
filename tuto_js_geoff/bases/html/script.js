326c43680b75d964b9a15b54311a04b5d69459b0
etienne1698 <etienne.robert1698@gmail.com>
2020-09-13 00:34:14 +0200

    js bases

--- tuto_js_geoff/bases/html/script.js
+++ tuto_js_geoff/bases/html/script.js
@@ -25,4 +25,9 @@ zone.innerHTML = '<h1>TEST</h1>' + zone.innerHTML;
 let li = document.getElementsByTagName('li');
 li[0].style.backgroundColor = 'red';
 
-
+// exemple 3
+let button = document.getElementById('consoleMessage');
+let input = document.getElementById('consoleMessageValue');
+button.addEventListener('click', function () {
+    console.log(input.value)
+});
