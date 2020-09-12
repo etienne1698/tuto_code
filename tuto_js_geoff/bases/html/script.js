adc91a02d9137bb7940adab04bba9ebb6779bc9a
etienne1698 <etienne.robert1698@gmail.com>
2020-09-12 19:17:38 +0200

    tutos js

---
+++ tuto_js_geoff/bases/html/script.js
@@ -0,0 +1,28 @@
+// exemple 1
+let monTitre = document.getElementById('mon_titre');
+monTitre.innerHTML = 'Bete de titre';
+monTitre.style.backgroundColor = 'red';
+
+/*
+ 5 == "5"     true
+ 5 === "5"    false
+
+ */
+setInterval(() => {
+    if (monTitre.style.backgroundColor === 'red') {
+        monTitre.style.backgroundColor = 'blue';
+    } else {
+        monTitre.style.backgroundColor = 'red';
+    }
+    //monTitre.style.backgroundColor === 'red' ?  monTitre.style.backgroundColor = 'blue' :  monTitre.style.backgroundColor = 'red';
+}, 1000);
+
+
+// exemple 2
+let zone = document.getElementById('zone');
+zone.innerHTML = '<h1>TEST</h1>' + zone.innerHTML;
+
+let li = document.getElementsByTagName('li');
+li[0].style.backgroundColor = 'red';
+
+
